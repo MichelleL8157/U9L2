5b3ecb92da06600bf80b2668ac9c5c4bba74be5e
BT_1E10_10 <eleanorc25@nycstudents.net>
2022-03-15 12:10:15 -0400

    deleted unnecessary classes

--- src/Q3_KFC.java
+++ src/Q3_KFC.java
@@ -1,7 +1,7 @@
 public class Q3_KFC extends Q3_Store{
     private int chickenCount;
 
-    public Q3_KFC((String name, int employees, boolean isOpen, int chickenCount) {
+    public Q3_KFC(String name, int employees, boolean isOpen, int chickenCount) {
         super(name, employees, isOpen);
         this.chickenCount = chickenCount;
     }
